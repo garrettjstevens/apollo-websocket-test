28b7e93809fc533a3e2d1ff039689e14af964e4e
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-03-11 11:26:29 -0700

    First try at connecting to Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import SockJS from 'sockjs-client'
 
 function App() {
+  const [output, setOutput] = useState('')
+  const [errorOutput, setErrorOutput] = useState('')
+  const [socket, setSocket] = useState<WebSocket>()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', width: 200 }}>
+        Apollo Web Socket Connection
+        <button onClick={() => {
+          let socket: WebSocket | undefined
+          try {
+            // Use this instead to try with native web socket
+            // socket = new WebSocket('ws://demo.genomearchitect.org/Apollo2/stomp')
+            socket = new SockJS('http://demo.genomearchitect.org/Apollo2/stomp')
+          } catch (error) {
+            setErrorOutput(errorOutput + String(error))
+          }
+          if (!socket) {
+            return
+          }
+          console.log(socket)
+
+          socket.onmessage = (e) => {
+            console.log(e.data)
+            setOutput(output + String(e.data))
+          }
+
+          socket.onopen = () =>{
+            console.log('opening...')
+            socket && socket.send('hello server')
+          }
+
+          socket.onclose = () => {
+            console.log('closing...')
+            setSocket(undefined)
+          }
+
+          socket.onerror = (error) => {
+            setErrorOutput(errorOutput + String(error))
+            console.error(error)
+            console.dir(error)
+          }
+          setSocket(socket)
+        }}>Connect</button>
+        <button onClick={() => {
+          socket && socket.close()
+        }} disabled={!socket}>Disconnect</button>
+      </div>
+      <h6>Output</h6>
+      <textarea>{output}</textarea>
+      <h6>Errors</h6>
+      <textarea>{errorOutput}</textarea>
     </div>
   );
 }
