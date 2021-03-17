e189bd1d1b37d4e26c389f77f08cd106973ff3fd
Nathan Dunn <nathandunn@lbl.gov>
2021-03-17 08:36:38 -0700

    fixed by putting everything in a semi-constructor

--- src/App.tsx
+++ src/App.tsx
@@ -5,73 +5,86 @@ import './App.css';
 import {Stomp} from "@stomp/stompjs";
 // import SockJS from 'sockjs-client';
 
-let sock : any
+// let sock : any
 let client : any
 
-function App() {
-  const [output, setOutput] = useState('')
-  const [errorOutput, setErrorOutput] = useState('')
-  const [socket, setSocket] = useState<WebSocket>()
 
-  // sock = new SockJS('http://localhost:8080/apollo/stomp')
-  sock = new WebSocket('ws://localhost:8080/apollo/stomp/websocket')
-  // client = Stomp.over(sock);
-  client = Stomp.over(function(){
-    return new WebSocket('ws://localhost:8080/apollo/stomp/websocket')
-  });
-  client.reconnectDelay = 300
-    client.debug = function (str:string) {
-        console.log(str)
-    };
-  client.onDisconnect  = function(){
-      console.log('disconnected')
-  }
-    client.onWebSocketClose = function(){
-        console.log('websocket closed')
-    }
-
-  let username:any = undefined
-    const urlParams = new URLSearchParams(window.location.search);
-  if(urlParams.has('username')){
-      username = urlParams.get('username')
-  }
-
-  console.log('usernames',username)
-
-  // client.send()
-  console.log('client',client)
+const App = () => {
 
 
-    client.onConnect = function(frame:any){
-        console.log('client connected',client.connected)
-        console.log('getting frame',frame)
-        client.subscribe("/topic/AnnotationNotification", function (message:any) {
-            console.log('listening to main topic')
-            console.log(message)
-            console.log(message.binaryBody)
-            console.log(message.body)
-            console.log(message.headers)
-            const finalOutput = `body\n======\n${message.body}\n====\ntype: ${message.binaryBody ? 'binary' : 'text' }\nheader:\n${JSON.stringify(message.headers)}\n=====\n`
-            setOutput(finalOutput)
+    const [output, setOutput] = useState('')
+    const [errorOutput, setErrorOutput] = useState('')
+    const [socket, setSocket] = useState<WebSocket>()
+    const [constructorHasRun, setConstructorHasRun] = useState(false);
+
+
+    const constructor = () => {
+        if (constructorHasRun) return;
+        // sock = new SockJS('http://localhost:8080/apollo/stomp')
+        // sock = new WebSocket('ws://localhost:8080/apollo/stomp/websocket')
+        // client = Stomp.over(sock);
+        client = Stomp.over(function(){
+            return new WebSocket('ws://localhost:8080/apollo/stomp/websocket')
         });
-      if(username){
-          client.subscribe(`/topic/AnnotationNotification/user/${username}`, function (message:any) {
-              console.log('listening to user topic')
-              const finalOutput = `body\n======\n${message.body}\n====\ntype: ${message.binaryBody ? 'binary' : 'text' }\nheader:\n${JSON.stringify(message.headers)}\n=====\n`
-              console.log(finalOutput)
-              setOutput(finalOutput)
-          });
-      }
-    }
+        // client.reconnectDelay = 300
+        // client.debug = function (str:string) {
+        //     console.log(str)
+        // };
+        client.onDisconnect  = function(){
+            console.log('disconnected')
+        }
+        client.onWebSocketClose = function(){
+            console.log('websocket closed')
+        }
 
-    client.onStompError = function(frame:any){
-        console.error('Broker reported error: ' + frame.headers['message']);
-        console.error('Additional details: ' + frame.body);
-        setErrorOutput(frame.headers['message'])
-  }
+        let username:any = undefined
+        const urlParams = new URLSearchParams(window.location.search);
+        if(urlParams.has('username')){
+            username = urlParams.get('username')
+        }
+
+        console.log('usernames',username)
+
+        // client.send()
+        console.log('client',client)
 
 
-  client.activate()
+        client.onConnect = function(frame:any){
+            console.log('client connected',client.connected)
+            console.log('client connected state',client.state)
+            console.log('getting frame',frame)
+            client.subscribe("/topic/AnnotationNotification", function (message:any) {
+                console.log('listening to main topic')
+                console.log(message)
+                console.log(message.binaryBody)
+                console.log(message.body)
+                console.log(message.headers)
+                const finalOutput = `body\n======\n${message.body}\n====\ntype: ${message.binaryBody ? 'binary' : 'text' }\nheader:\n${JSON.stringify(message.headers)}\n=====\n`
+                setOutput(finalOutput)
+            });
+            if(username){
+                client.subscribe(`/topic/AnnotationNotification/user/${username}`, function (message:any) {
+                    console.log('listening to user topic')
+                    const finalOutput = `body\n======\n${message.body}\n====\ntype: ${message.binaryBody ? 'binary' : 'text' }\nheader:\n${JSON.stringify(message.headers)}\n=====\n`
+                    console.log(finalOutput)
+                    setOutput(finalOutput)
+                });
+            }
+        }
+
+        client.onStompError = function(frame:any){
+            console.error('Broker reported error: ' + frame.headers['message']);
+            console.error('Additional details: ' + frame.body);
+            setErrorOutput(frame.headers['message'])
+        }
+
+
+        client.activate()
+        setConstructorHasRun(true);
+    };
+
+    constructor()
+
 
 
   return (
