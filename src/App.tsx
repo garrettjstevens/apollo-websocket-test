43136c7d43ec6e7ac2fe4d0bb8c0b13f11d6016e
Garrett Stevens <stevens.garrett.j@gmail.com>
2021-03-22 16:18:35 -0600

    Add username and password fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 
-import { CompatClient, Stomp } from '@stomp/stompjs'
+import { Client, Frame, Message } from '@stomp/stompjs'
 
 const App = () => {
+  const [apolloUrl, setApolloUrl] = useState('http://localhost:8080/apollo')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
   const [output, setOutput] = useState('')
   const [errorOutput, setErrorOutput] = useState('')
-  const [client, setClient] = useState<CompatClient>()
+  const [client, setClient] = useState<Client>()
 
   useEffect(() => {
-    const c = Stomp.over(function () {
-      return new WebSocket('ws://localhost:8080/apollo/stomp/websocket')
+    return () => {
+      client && client.deactivate()
+    }
+  }, [client])
+
+  function onConnectClick() {
+    let url: URL
+    try {
+      url = new URL(apolloUrl)
+    } catch (error) {
+      setErrorMessage('URL is not valid')
+      return
+    }
+    url.protocol = url.protocol.startsWith('https') ? 'wss' : 'ws'
+    url.pathname += '/stomp/websocket'
+    url.search = `?username=${username}&password=${password}`
+    const c = new Client({
+      brokerURL: url.href,
     })
-    c.onDisconnect = function () {
+    c.onDisconnect = () => {
+      c.deactivate()
+      setClient(undefined)
       console.log('disconnected')
     }
-    c.onWebSocketClose = function () {
+    c.onWebSocketClose = (event) => {
+      setClient(undefined)
       console.log('websocket closed')
+      console.log(event)
+    }
+    c.onWebSocketError = (event) => {
+      setErrorMessage(
+        'Problem opening web socket, please check URL, username, and password'
+      )
     }
 
-    let username: any = undefined
-    const urlParams = new URLSearchParams(window.location.search)
-    if (urlParams.has('username')) {
-      username = urlParams.get('username')
-    }
-
-    console.log('usernames', username)
-
-    console.log('client', c)
-
-    c.onConnect = function (frame: any) {
+    c.onConnect = (frame: Frame) => {
       console.log('client connected', c.connected)
       console.log('client connected state', c.state)
       console.log('getting frame', frame)
-      c.subscribe('/topic/AnnotationNotification', function (message: any) {
+      c.subscribe('/topic/AnnotationNotification', (message: Message) => {
         console.log('listening to main topic')
         console.log(message)
         console.log(message.binaryBody)
@@ -47,7 +66,7 @@ const App = () => {
       if (username) {
         c.subscribe(
           `/topic/AnnotationNotification/user/${username}`,
-          function (message: any) {
+          (message: Message) => {
             console.log('listening to user topic')
             const finalOutput = `body\n======\n${message.body}\n====\ntype: ${
               message.binaryBody ? 'binary' : 'text'
@@ -57,12 +76,9 @@ const App = () => {
           }
         )
       }
-      return () => {
-        c.deactivate()
-      }
     }
 
-    c.onStompError = function (frame: any) {
+    c.onStompError = (frame: Frame) => {
       console.error('Broker reported error: ' + frame.headers['message'])
       console.error('Additional details: ' + frame.body)
       setErrorOutput(frame.headers['message'])
@@ -71,26 +87,82 @@ const App = () => {
     c.activate()
 
     setClient(c)
-  }, [])
-
-  if (!(client && client.active)) {
-    return null
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', width: 200 }}>
-        Apollo Web Socket Connection
+      <div style={{ display: 'flex', flexDirection: 'column', width: 240 }}>
+        <h4>Apollo Web Socket Connection</h4>
+        <label style={{ marginBottom: 40 }}>
+          Apollo URL:
+          <input
+            type="text"
+            size={60}
+            value={apolloUrl}
+            onChange={(event) => {
+              setApolloUrl(event.target.value)
+              setErrorMessage('')
+            }}
+            disabled={client && client.active}
+          />
+        </label>
+        <label style={{ marginBottom: 40 }}>
+          Username:
+          <input
+            type="text"
+            size={60}
+            value={username}
+            onChange={(event) => {
+              setUsername(event.target.value)
+              setErrorMessage('')
+            }}
+            disabled={client && client.active}
+          />
+        </label>
+        <label style={{ marginBottom: 40 }}>
+          Password:
+          <input
+            type="password"
+            size={60}
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value)
+              setErrorMessage('')
+            }}
+            disabled={client && client.active}
+          />
+        </label>
         <button
-          disabled={!client.active}
+          disabled={(client && client.active) || !username || !password}
+          onClick={onConnectClick}
+        >
+          Connect
+        </button>
+        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+        <button
+          onClick={() => {
+            client && client.active && client.deactivate()
+          }}
+          disabled={!(client && client.active)}
+        >
+          Disconnect
+        </button>
+        <hr style={{ width: '200%' }} />
+        <button
+          disabled={!(client && client.active)}
           onClick={() => {
             try {
               console.log('sending')
-              console.log('connected', client.connected, client.active)
-              client.publish({
-                destination: '/app/AnnotationNotification',
-                body: JSON.stringify({ input: 'output', operation: 'ping' }),
-              })
+              console.log(
+                'connected',
+                client && client.connected,
+                client && client.active
+              )
+              client &&
+                client.publish({
+                  destination: '/app/AnnotationNotification',
+                  body: JSON.stringify({ input: 'output', operation: 'ping' }),
+                })
               console.log('sent')
             } catch (error) {
               setErrorOutput(errorOutput + String(error))
@@ -100,17 +172,20 @@ const App = () => {
           Test Send
         </button>
         <button
-          disabled={!client.active}
+          disabled={!(client && client.active)}
           onClick={() => {
             try {
-              console.log('client connected', client.connected)
+              console.log('client connected', client && client.connected)
 
               console.log('sending')
-              client.send(
-                '/app/AnnotationNotification',
-                {},
-                JSON.stringify({ input: 'output', operation: 'broadcast' })
-              )
+              client &&
+                client.publish({
+                  destination: '/app/AnnotationNotification',
+                  body: JSON.stringify({
+                    input: 'output',
+                    operation: 'broadcast',
+                  }),
+                })
               console.log('sent')
             } catch (error) {
               setErrorOutput(errorOutput + String(error))
@@ -118,14 +193,6 @@ const App = () => {
           }}
         >
           Broadcast Test
-        </button>
-        <button
-          onClick={() => {
-            client.active && client.deactivate()
-          }}
-          disabled={!client.active}
-        >
-          Disconnect
         </button>
       </div>
       <h6>Output</h6>
